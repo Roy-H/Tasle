ec5782d477ff354fc68601c2e72a13c170f9ec81
Roy-H <1246799927@qq.com>
2018-07-11 18:19:09 +0800

    first commit

---
+++ Tasle/DataHolder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tasle
+{
+    public class DataHolder
+    {
+        public byte[] mRecvDataCache;
+        public byte[] mRecvData;
+
+        private int mTail = -1;
+        private int mPackLen;
+        private const int mPackBytes = 1;
+
+        public int Count
+        {
+            get
+            {
+                return mTail + 1;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return mRecvDataCache != null ? mRecvDataCache.Length : 0;
+            }
+        }
+
+        public void Push(byte[] data, int lenght)
+        {
+            if (mRecvDataCache == null)
+                mRecvDataCache = new byte[lenght];
+
+            if (Count + lenght > Capacity)
+            {
+                byte[] newArr = new byte[this.Count + lenght];
+                mRecvDataCache.CopyTo(newArr, 0);
+                mRecvDataCache = newArr;
+            }
+            Array.Copy(data, 0, mRecvDataCache, mTail + 1, lenght);
+            mTail += lenght;
+        }
+
+        public bool IsFinished()
+        {
+            if (Count == 0)
+                return false;
+
+            if (Count > 0)
+            {
+                DataStream reader = new DataStream(mRecvDataCache, true);
+
+                mPackLen = (int)reader.ReadByte();
+                //more than mPackBytes lenght and process
+                if (mPackLen > mPackBytes)
+                {
+                    if (Count - mPackBytes >= mPackLen)
+                    {
+                        mRecvData = new byte[mPackLen];
+                        Array.Copy(mRecvDataCache,1,mRecvData,0,mPackLen);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Tasle/SocketHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Tasle
+{
+    public sealed class SocketHelper
+    {
+        
+        private static object sLock = new object();
+        private static SocketHelper sSocketHelper;
+        private int mTimeOut = 5000;
+
+        public delegate void ConnectCallback();
+
+        ConnectCallback mConnectCallback = null;
+        ConnectCallback mConnectFailedCallback = null;
+        private Socket socket;
+
+        public static SocketHelper Instance
+        {
+            get
+            {
+                if (sSocketHelper == null)
+                {
+                    lock (sLock)
+                    {
+                        if (sSocketHelper == null)
+                            sSocketHelper = new SocketHelper();
+                    }
+                }
+                return sSocketHelper;
+            }
+        }
+
+
+        //using Tcp as Connect protocol
+        public void Connect(string serverIp, int serverPort, 
+            ConnectCallback connectCallback, 
+            ConnectCallback connectFailedCallback
+            )
+        {
+            mConnectCallback = connectCallback;
+            mConnectFailedCallback = connectFailedCallback;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address = IPAddress.Parse(serverIp);
+            IPEndPoint endpoint = new IPEndPoint(address, serverPort);
+
+            IAsyncResult result = socket.BeginConnect(endpoint, new AsyncCallback(ConnectedCallback), socket);
+
+            //timeout detecting
+            bool success = result.AsyncWaitHandle.WaitOne(mTimeOut, true);
+            if (!success)
+            {
+                //time out occurs
+                mConnectFailedCallback?.Invoke();
+            }
+            else
+            {
+                StartReceive();
+            }
+
+        }
+
+        private void StartReceive()
+        {
+            Thread thread = new Thread(new ThreadStart(ReceiveSocket));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void ReceiveSocket()
+        {
+
+        }
+
+        private void ConnectedCallback(IAsyncResult asyncConnect)
+        {
+            if (!socket.Connected)
+            {
+                mConnectFailedCallback?.Invoke();
+                return;
+            }
+
+            mConnectCallback?.Invoke();
+        }
+    }
+}
